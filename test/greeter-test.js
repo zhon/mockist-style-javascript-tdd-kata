75a27ab065d9da9c9bc2960866ada12ce8bc92ff
zhon <zhon@xputah.org>
2012-06-21 13:57:22 -0600

    All coded up

--- test/greeter-test.js
+++ test/greeter-test.js
@@ -1,7 +1,118 @@
 "use strict";
 
 buster.testCase("Greeter", {
-    "calls console log with hello": function () {
 
+    "uses voice to greet": function() {
+        var voice = this.stub();
+        var greeter = CreateGreeter(voice);
+        greeter.greet();
+        assert.called(voice);
+        assert.match(voice.firstCall.args[0], /Hello/);
     }
+    ,
+    "listens with an ear": function () {
+        var ear = this.stub();
+        var greeter = CreateGreeter(null, ear);
+        greeter.listen();
+        assert.called(ear);
+    }
+    ,
+    "sends a pearl of wisdom": function () {
+        var voice = this.stub();
+        var ear = this.stub();
+        var guru = function (callback) { callback('a nugget of wisdom'); }
+        var greeter = CreateGreeter(voice, ear, guru);
+        greeter.greet("Kent");
+        greeter.pontificate();
+        assert.calledTwice(voice);
+        assert.match(voice.secondCall.args[0], /Kent.*wisdom/);
+    }
+    ,
+    "pontificates guru wisdom": function() {
+        var voice = this.stub();
+        var ear = this.stub();
+        var guru = this.stub();
+        var greeter = CreateGreeter(voice, ear, guru);
+        greeter.pontificate();
+        assert.called(guru);
+    }
+
+});
+
+buster.testCase("Voice", {
+
+    "speaks to the DOM": function() {
+        this.stub(jQuery.prototype, "html");
+        this.stub(jQuery.prototype, "show");
+        voice('sup');
+        assert.calledWith(jQuery.prototype.html, 'sup');
+    }
+    ,
+    "shows the voicebox": function() {
+        this.stub(jQuery.prototype, "html");
+        this.stub(jQuery.prototype, "show");
+        voice("howdy, y'all");
+        assert.called(jQuery.prototype.show);
+    }
+    ,
+    "hides the voicebox after a few seconds": function() {
+        this.stub(jQuery.prototype, "html");
+        this.stub(jQuery.prototype, "show");
+        this.stub(jQuery.prototype, "slideUp");
+        var clock = this.useFakeTimers();
+
+        voice("goodbye");
+        refute.called(jQuery.prototype.slideUp);
+        clock.tick(5000)
+
+        assert.called(jQuery.prototype.slideUp);
+    }
+    ,
+    "greets a person": function() {
+        var voice = this.stub();
+        var greeter = CreateGreeter(voice);
+        greeter.greet("Bob");
+        assert.called(voice);
+        assert.match(voice.firstCall.args[0], /Bob/);
+    }
+
+});
+
+
+buster.testCase("Ear", {
+
+    "//notifies after hearing something": function(done) {
+        this.stub(jQuery.prototype, "show");
+        var callback = this.spy(function(sound) {
+            assert.equals(typeof sound, "string");
+            done();
+        });
+        ear(callback);
+        assert.called(callback);
+    }
+    ,
+    "shows the earBox": function() {
+        this.stub(jQuery.prototype, "show");
+        ear(this.stub());
+        assert.called(jQuery.prototype.show);
+    }
+    ,
+    "watches for a submit event": function () {
+        this.stub(jQuery.prototype, "show");
+        this.stub(jQuery.prototype, "submit");
+        ear(this.stub());
+        assert.called(jQuery.prototype.submit);
+    }
+
+});
+
+buster.testCase("Guru", {
+
+    "shares wisdom in a callback": function(done) {
+        guru(function(wisdom) {
+            assert.equals(typeof wisdom, 'string');
+            done();
+        });
+    }
+
 });
